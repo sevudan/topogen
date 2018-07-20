d910806449d8f3dd7957052a7ce674d40f9c6077
sevudan <sevudan.mk@yandex.ru>
2018-07-20 15:07:17 +0300

    first draft

---
+++ topo.py
@@ -0,0 +1,17 @@
+#!/usr/sbin/python3.5
+
+
+import networkx as nx
+import matplotlib as plt
+
+def __main__():
+    pass
+
+def gentopo():
+    G = nx.Graph()
+
+
+
+
+
+G.add_nodes_from(['R1','R2','R3','RR'])
