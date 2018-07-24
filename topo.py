0ee17e2d682d2a99735330d3c43e313a7853b07d
sevudan <sevudan.mk@yandex.ru>
2018-07-24 14:19:48 +0300

    Update file topo and add new file.

--- topo.py
+++ topo.py
@@ -14,48 +14,49 @@ def topology():
     """
     total_nodes = 5
     number_of_rr = 1
+    onerouter = True
     if number_of_rr > 0:
         G = nx.Graph(name='Network topology - Star')
         starTopology(G,number_of_rr)
     else: 
-        G = nx.Graph(name='Network topology - Full fullMeshTopology')
+        G = nx.complete_graph(total_nodes)
         fullMeshTopology(G)
-    return G.edges()
+    return G.edges(), onerouter
 
-def starTopology(G, number_of_rr,total_nodes):
+def genEdge(i, number_of_rr, total_nodes, onerouter):
     """
-    Function used for create a Graph of type a star.
+    Create edges for Graph.
     """
-    i = 1
+    if onerouter is True:    
+        i = 0
+        while i < total_nodes:
+            if number_of_rr == 0:
+                nx.set_node_attributes(G, {i:{'role':'Router'}})
+                nx.set_edge_attributes(G, {(i,0): {'ifd':'ge-0/0/1'}})
+                nx.set_edge_attributes(G, {(0,i): {'ifd':'ge-0/0/{}'.format(i)}})
+                i += 1
+            else:
+                nx.set_edge_attributes(G, {(0,i): {'ifd':'ge-0/0/{}'.format(i)}})
+                i += 1
+    else:
+        pass
+
+def starTopology(G, number_of_rr, total_nodes, onerouter):
+    """
+    Function used for create Graph of type a star.
+    """
     nodes = list(range(0,total_nodes + 1))
     nx.add_star(G, nodes)
     nx.set_node_attributes(G, {0: {'type':'Route-Reflector'}})
-    while i < total_nodes:
-        nx.set_node_attributes(G, {i:{'role':'Router'}})
-        nx.set_edge_attributes(G, {(i,0): {'ifd':'ge-0/0/1'}})
-        nx.set_edge_attributes(G, {(0,i): {'ifd':'ge-0/0/{}'.format(i)}})
-        i += 1
-    return G
+    i = 1
+    genEdge(i, number_of_rr, total_nodes)
 
-def fullMeshTopology(G):
-    pass
-
-
-"""
-while i < total_nodes:
-    nodes = list(range(i,total_nodes + 1))
-    if number_of_rr != 0:
-        G.add_node(number_of_rr, role="Route-Reflector")
-        for router in nodes:
-            G.add_edge(number_of_rr,router,self_ifd="ge-0/0/{}".format(router))
-        number_of_rr = 0
-        i += 2
-    else:
-        G.add_nodes_from(nodes, role="Router")
-        for router in nodes:
-            G.add_edge(router,number_of_rr,self_ifd="ge-0/0/{}".format(number_of_rr)) 
-        i = 5
-"""
+def fullMeshTopology(G, total_nodes):
+    """
+    Generate new topology type of Full Mesh.
+    """
+    i = 0
+    genEdge(i, number_of_rr, total_nodes)
 
 
 nx.draw(G, with_labels=True)
