beb5ec2c51fc10a3c8df434870c485dc28eebdf6
sevudan <makstill@gmail.com>
2018-08-29 23:39:11 +0300

    bad work

--- topo.py
+++ topo.py
@@ -27,25 +27,24 @@ def topology():
         attr = param()
         core_nodes = attr['core_nodes']
         total_ce = attr['total_ce']
+        total_rr = attr['total_rr']
         ls = attr['ls']
         if total_ce > 0:
             nodes_ce = sorted(map(
                                 lambda x: 
-                                    'CE{}'.format(x),range(0,total_ce)
+                                    'CE{}'.format(x), range(0, total_ce)
                             ))
         #Generate nodes with name 'RX', where X is serial number 
         nodes = sorted(map(
                         lambda x:
-                            'R{}'.format(x),range(0,core_nodes)
+                            'R{}'.format(x), range(0, core_nodes)
                         ))
-        if totalRR > 0:
+        if total_rr > 0:
             G = nx.DiGraph(name='Network topology - Star')
             nx.add_star(G, nodes)
-            gen_core_nodes(G, nodes, core_nodes, nodes_ce, ls)
             gen_edge(G, nodes, core_nodes, nodes_ce, ls)
         else: 
             G = nx.complete_graph(core_nodes)
-            gen_core_nodes(G, nodes, core_nodes, nodes_ce, ls)
             gen_edge(G, nodes, core_nodes, nodes_ce, ls)
         return G
     except AddressValueError as value_error:
@@ -57,18 +56,18 @@ def param():
     try:
         # chek ip_address
         lo_pool = str(IPv4Network('192.168.10.0/24'))
-        ifpool = str(IPv4Network('10.250.0.0/23'))
+        if_pool = str(IPv4Network('10.250.0.0/23'))
         # set other params
         core_nodes = 5 
-        totalRR = 1
+        total_rr = 1
         total_ce = 2
         ls = True
-        nodes = sum([core_nodes,total_ce])
-        if nodes > 16:
-            nodes/0 
+        sum_nodes = sum([core_nodes, total_ce])
+        if sum_nodes > 16:
+            sum_nodes/0 
         else:
-            attr = {'lo_pool':lo_pool, 'ifpool':ifpool, 'nodes':nodes,
-                    'core_nodes':core_nodes, 'totalRR':totalRR,
+            attr = {'lo_pool':lo_pool, 'ifpool':if_pool, 'sum_nodes':sum_nodes,
+                    'core_nodes':core_nodes, 'total_rr':total_rr,
                     'total_ce':total_ce, 'ls':ls}
         return attr
     except AddressValueError as value_error:
@@ -76,34 +75,6 @@ def param():
     except ZeroDivisionError as error:
         return error
 
-def gen_core_nodes(G, nodes, core_nodes, ls):
-    """
-    Function for creating CORE nodes and attributes.
-    """
-    var = param()
-    lo_pool = var['lo_pool']
-    loopbacks = list(net.gen_loopback(lo_pool))[:core_nodes]
-    # set loopback addres for RR
-    nx.set_node_attributes(G, {'R0': {'type':'Route-Reflector', 'loopback':loopbacks.pop(0)}}) 
-    # set loopback addres for other nodes
-    [*map(
-        lambda node, loopback:
-            nx.set_node_attributes(G, {node: {'loopback':loopback}}), 
-            nodes[1:], loopbacks
-        )]            
-    return G
-
-def gen_edge_node(G, nodes, nodes_ce, ls):
-    """
-    Function for creating edges for nodes and attributes.
-    """
-    total_ce = len(nodes_ce)
-    # choice CORE nodes for peers.
-    pe_peers = random.sample(nodes, total_ce)
-    edges_to_ce = list(zip(pe_peers, 
-                        map(lambda x: 'CE{}'.format(x),range(total_ce)))
-                        )
-    G.add_edges_from(edges_to_ce)
 
 def gen_edge(G, nodes, core_nodes, nodes_ce, ls):
 
@@ -113,12 +84,15 @@ def gen_edge(G, nodes, core_nodes, nodes_ce, ls):
     """
 
     ifd = 'lt' if ls else 'ge'
-    # To determine new edges for nodes between RR and R routers.
+    # To determine new edges for nodes between RR, PE and CE routers.
     edges_to_r = list(zip(map(lambda x: 'R0',nodes[1:]),
                         map(lambda x: '{}'.format(x),nodes[1:]))
                         )
     edges_to_rr = list(zip(nodes[1:], 
                         map(lambda x: 'R{}'.format(0),nodes[1:]))
+                        )    
+    edges_to_ce = list(zip(pe_peers, 
+                        map(lambda x: 'CE{}'.format(x),range(total_ce)))
                         )
     # Create new edges from R to RR.
     G.add_edges_from(edges_to_rr)
@@ -129,6 +103,31 @@ def gen_edge(G, nodes, core_nodes, nodes_ce, ls):
     units = gen_edge_unit(total_edges)
     # Set interface and ip address for interface between RR and other routers.    
     ifl_num = range(1, len(edges_to_r)+1)
+    
+    return G
+
+def gen_edge_attr(G, nodes, nodes_ce, ls):
+    """
+    Function for creating attributes for edges and nodes.
+    """
+    attr = param()
+    lo_pool = attr['lo_pool']
+    sum_node = attr['sum_nodes']
+    loopbacks = list(net.gen_loopback(lo_pool))[:sum_node]
+    # set loopback addres for RR
+    if total_rr > 0:
+        nx.set_node_attributes(G, {'R0': {'type':'Route-Reflector', 'loopback':loopbacks.pop(0)}}) 
+    # set loopback addres for other nodes
+    [*map(
+        lambda node, loopback:
+            nx.set_node_attributes(G, {node: {'loopback':loopback}}), 
+            nodes[1:], loopbacks
+        )]
+
+    total_ce = len(nodes_ce)
+    # choice CORE nodes for peers.
+    pe_peers = random.sample(nodes, total_ce)
+    G.add_edges_from(edges_to_ce)
     [*map(
         lambda edges,ifl_num, ifa, unit, r_unit:
             nx.set_edge_attributes(G, 
@@ -150,7 +149,6 @@ def gen_edge(G, nodes, core_nodes, nodes_ce, ls):
             ),
             edges_to_rr, pool['neighbor'], units['r_unit'], units['l_unit']
         )]
-    return G
 
 def gen_edge_addr(core_nodes):
 
@@ -159,8 +157,8 @@ def gen_edge_addr(core_nodes):
     '''
 
     var = param()
-    ifpool = var[ifpool]
-    pool = list(net.gen_ifaddress(ifpool))[:core_nodes-1]
+    if_pool = var['ifpool']
+    pool = list(net.gen_ifaddress(if_pool))[:core_nodes-1]
     local_ifa = [ str(x[0]) for x in pool ]
     neighbor_ifa = [ str(x[1]) for x in pool ]
     return {'local':local_ifa, 'neighbor':neighbor_ifa}
